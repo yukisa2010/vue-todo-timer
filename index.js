c77e3a23525fa868d0a51dc2949ee79b50096c85
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-11-26 00:18:38 +0900

    localStorage completed!

--- index.js
+++ index.js
@@ -34,6 +34,22 @@ Vue.component('form-wrapper', {
 
 
 Vue.component('todo-items', {
+  template: `
+  <ul>
+    <li v-for="todo in todos" v-if="!todo.done">
+        <input type="checkbox" v-model="todo.done"/>
+        {{ todo.title }}
+        <button @click="beginTimer(todo.id)" :disabled="!!todo.timerId">begin</button>
+        <button @click="endTimer(todo.id)">end</button>
+      timer:  {{ todo.timerCount }}
+    </li>
+    <li v-for="todo in todos" v-if="todo.done" class="done">
+        {{ todo.title }}
+        timer:  {{ todo.timerCount }}
+        <button @click="restoreTodo(todo.id)">restore</button>
+    </li>
+  </ul>
+  `,
   data: function() {
     return {
       todos: [
@@ -44,6 +60,7 @@ Vue.component('todo-items', {
     }
   },
   mounted: function() {
+    this.todos = JSON.parse(localStorage.getItem('key'))
     bus.$on('insertTodo', this.insertItem)
   },
   methods: {
@@ -68,18 +85,27 @@ Vue.component('todo-items', {
           self.todos[index].done = true
         }
       })
-      clearInterval()
+      this.setTodosToLocal()
     },
     insertItem: function(newTitle) {
-      let maxId = 0;
-      this.todos.forEach(function(todo) {
-        if(maxId < todo.id) {
-          maxId = todo.id
-        }
-      })
+      let maxId = 0
+
+      if(this.todos !== null) {
+        maxId = this.todos.reduce(function(max, todo, index) {
+          if(max.id < todo.id) {
+            return { id: todo.id }
+          } else {
+            return { id: max.id }
+          } 
+        }).id
+      }
+
+      if(this.todos == null) this.todos = []
+
       this.todos.push({
-        id: maxId + 1, title: newTitle, done: false, timerCount: 0, timerId: 0
+          id: maxId + 1, title: newTitle, done: false, timerCount: 0, timerId: 0
       })
+      this.setTodosToLocal()  
     },
     restoreTodo(id) {
       //done を falseに戻す
@@ -90,24 +116,12 @@ Vue.component('todo-items', {
           self.todos[index].done = false
         }
       })
+      this.setTodosToLocal()
+    },
+    setTodosToLocal() {
+      localStorage.setItem('key' ,JSON.stringify(this.todos))
     }
-  },
-  template: `
-  <ul>
-    <li v-for="todo in todos" v-if="!todo.done">
-        <input type="checkbox" v-model="todo.done"/>
-        {{ todo.title }}
-        <button @click="beginTimer(todo.id)" :disabled="!!todo.timerId">begin</button>
-        <button @click="endTimer(todo.id)">end</button>
-      timer:  {{ todo.timerCount }}
-    </li>
-    <li v-for="todo in todos" v-if="todo.done" class="done">
-        {{ todo.title }}
-        timer:  {{ todo.timerCount }}
-        <button @click="restoreTodo(todo.id)">restore</button>
-    </li>
-  </ul>
-  `
+  }
 })//todo-items
 
 
